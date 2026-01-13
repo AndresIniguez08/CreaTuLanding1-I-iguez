b95567dd160981d3414729999649f3e146454ad6
Andrés Iñiguez <andy.iniguez@hotmail.com>
2026-01-13 18:42:55 -0300

    no habia corrijo la llamada vieja al import del ShoppingCart

--- Primera entrega/src/components/CartWidget.jsx
+++ Primera entrega/src/components/CartWidget.jsx
@@ -5,7 +5,7 @@ const CartWidget = () => {
 
   return (
     <div className="relative">
-      <ShoppingCartIcon className="h-6 w-6 text-gray-800 cursor-pointer" />
+      <ShoppingCart className="h-6 w-6 text-gray-800 cursor-pointer" />
       <span className="absolute -top-2 -right-2 bg-gray-900 text-white text-xs font-semibold rounded-full px-1.5">
         {cartCount}
       </span>
