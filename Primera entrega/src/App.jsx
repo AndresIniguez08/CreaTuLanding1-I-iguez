2b50810102f14877a3eebf0cfb2f810f69dbb70a
Andrés Iñiguez <andy.iniguez@hotmail.com>
2026-02-03 18:43:54 -0300

    empiezo a trabajar en la routas

--- Primera entrega/src/App.jsx
+++ Primera entrega/src/App.jsx
@@ -1,11 +1,28 @@
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
+import { Routes, Route } from "react-router-dom";
+import ItemDetailContainer from "./components/ItemDetailContainer";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <NavBar />
-      <ItemListContainer greeting="¡Bienvenido a Fox Stickers! Elegí tus favoritos." />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ItemListContainer greeting="¡Bienvenido a Fox Stickers! Elegí tus favoritos." />
+          }
+        />
+        <Route path="/category/:categoryId" element={<ItemListContainer />} />
+
+        <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+
+        <Route
+          path="*"
+          element={<h2 className="p-4">404 - Página no encontrada</h2>}
+        />
+      </Routes>
     </div>
   );
 }
